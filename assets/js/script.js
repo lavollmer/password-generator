85a6c1fd344dd4ad355bb650c6ac7bb7ad96070f
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-09-29 07:52:50 -0500

    added random number generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,19 @@ var generateBtn = document.querySelector("#generate");
 
 //Write below this for CODE
 
+function getARandomNumber() {
+  return Math.floor(Math.random() * (128 - 8) + 8);
+}
+
+getARandomNumber()
+
 //Received code from Gary in-class
 function generatePassword() {
-
   var numberOfCharacters = prompt("How many characters do you want?")
   var includeNumbers = confirm("Do you want numbers?")
   var includeSpecialCharacters = confirm("Do you want special characters?")
+  var upperCaseletters = confirm("Do you want uppercase letters?")
+  var lowerCaseletters = confirm("Do you want lowercase letters?")
 
   var RandomNumber = getARandomNumber()
   finalPassword = finalPassword + RandomNumber;
