9b179ee35879973251a572f1ceb8e6131b452b58
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-09-29 08:13:50 -0500

    working on random character and number generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,41 @@
 var generateBtn = document.querySelector("#generate");
 
 //Write below this for CODE
+var numberOfCharacters = "";
+var includeNumbers = "";
+var includeSpecialCharacters = "";
+var upperCaseletters = "";
+var lowerCaseletters = "";
 
-function getARandomNumber() {
-  return Math.floor(Math.random() * (128 - 8) + 8);
+function getARandomNumber(includeNumbers) {
+  return Math.floor(Math.random(includeNumbers) * (128 - 8) + 8);
 }
 
-getARandomNumber()
+function getARandomCharacter() {
+  var charac = "abcdefghijklmnopqrstuvwxyz"
+  var characlength = charac.length;
+  return Math.floor(Math.random(characlength) * (26 - 1) + 1);
+}
+
+function getASpecialCharacter() {
+  var specialCharacter = "!@#$%^&*"
+  var specialCharacterLength = specialCharacter.length;
+  return Math.floor(Math.random(specialCharacterLength) * (8 - 1) + 1);
+}
 
 //Received code from Gary in-class
 function generatePassword() {
-  var numberOfCharacters = prompt("How many characters do you want?")
-  var includeNumbers = confirm("Do you want numbers?")
-  var includeSpecialCharacters = confirm("Do you want special characters?")
-  var upperCaseletters = confirm("Do you want uppercase letters?")
-  var lowerCaseletters = confirm("Do you want lowercase letters?")
+  numberOfCharacters = prompt("How many characters do you want? (minimum: 8 characters maximum: 128)")
+  includeNumbers = confirm("Do you want numbers?")
+  includeSpecialCharacters = confirm("Do you want special characters?")
+  upperCaseletters = confirm("Do you want uppercase letters?")
+  lowerCaseletters = confirm("Do you want lowercase letters?")
 
   var RandomNumber = getARandomNumber()
-  finalPassword = finalPassword + RandomNumber;
+  var RandomCharacter = getARandomCharacter()
+  return finalPassword = RandomNumber + RandomCharacter;
 }
+
 
 generatePassword()
 
