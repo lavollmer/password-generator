1357899e7bc5d058d8f101b78530239a9aed8e04
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-10-02 08:46:32 -0500

    final updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Write below this for CODE
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
-//I need to declare variables within a global scope to +- user input
-
-function userChoiceCharacters() {
-  numberOfCharacters = prompt("How many characters do you want? (minimum:8  maximum:128)")
-  console.log(numberOfCharacters);
-
-  includeNumbers = confirm("Do you want numbers?")
-  console.log(includeNumbers);
-
-  includeSpecialCharacters = confirm("Do you want special characters?")
-  console.log(includeSpecialCharacters);
-
-  upperCaseLetters = confirm("Do you want uppercase letters?")
-  console.log(upperCaseLetters);
-
-  lowerCaseLetters = confirm("Do you want lowercase letters?")
-  console.log(lowerCaseLetters);
-}
-
-userChoiceCharacters()
-
-
-//Received code from Gary in-class (lines 12 -17)
-function generatePassword() {
-  // I need to determine the length of the password based on user input
-  function generateCharacterLength() {
-    var x = numberOfCharacters * 1;
-    console.log(x);
-  }
-
-  //If user says yes (true), I need to create a random number
-  if (includeNumbers === true) {
-    function getARandomNumber() {
-      var x = (Math.floor(Math.random() * (100 - 0 + 1) + 0))
-    }
-  }
-
-  // If user says yes (true),I need to include a random special character
-  if (includeSpecialCharacters === true) {
-    function getASpecialCharacter() {
-      console.log(includeSpecialCharacters.random("!@#$%^&*"));
-    }
-  }
-
-  //If user says yes (true), I need to include a random upper case letter
-  if (upperCaseLetters === true) {
-    function getUpperCase() {
-      var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-      return alphabet.random()
-    }
-  }
-
-  // If user says yes(true), I need to include a random lower case number
-  if (lowerCaseLetters === true) {
-    function getLowercase() {
-
-    }
-  }
-
-  generateCharacterLength()
-  var RandomSpecialCharacter = getASpecialCharacter;
-  var RandomNumber = getARandomNumber()
-  var RandomUppercase = getUpperCase()
-  var RandomLowercase = getLowercase()
-
-  //generate final password with character length specified 
-  for (var x = 8; x < 128, x;) {
-    return finalPassword = RandomSpecialCharacter + RandomNumber + RandomUppercase + RandomLowercase;
-  }
-}
-
-generatePassword()
-
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -84,6 +11,91 @@ function writePassword() {
   passwordText.value = password;
 
 }
+//Write below this for CODE
+var numbersArray = ["1", "2", "3"];
+var characterPool = [];
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialCharacter = ["!", "@", "#"]
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  var numberOfCharacters = prompt("How many characters do you want? (minimum:8  maximum:128)")
+  console.log(numberOfCharacters);
+
+  var includeNumbers = confirm("Do you want numbers?")
+  console.log(includeNumbers);
+
+  var includeSpecialCharacters = confirm("Do you want special characters?")
+  console.log(includeSpecialCharacters);
+
+  var upperCaseLetters = confirm("Do you want uppercase letters?")
+  console.log(upperCaseLetters);
+
+  var lowerCaseLetters = confirm("Do you want lowercase letters?")
+  console.log(lowerCaseLetters);
+
+  // I need to determine the length of the password based on user input
+  // function generateCharacterLength() {
+  //   console.log(numberOfCharacters);
+  // }
+
+  //If user says yes (true), I need to create a random number
+  if (includeNumbers === true) {
+    // function getARandomNumber() {
+    //   var x = (Math.floor(Math.random() * (100 - 0 + 1) + 0))
+    // }
+    characterPool = characterPool.concat(numbersArray);
+  }
+
+  // If user says yes (true),I need to include a random special character
+  if (includeSpecialCharacters === true) {
+    // function getASpecialCharacter() {
+    //   var result = "";
+    //   var specialCharacterOption = "!@#$%^&*<>";
+    //   result = console.log(specialCharacterOption);
+    // }
+    characterPool = characterPool.concat(specialCharacter);
+  }
+
+  //If user says yes (true), I need to include a random upper case letter
+  if (upperCaseLetters === true) {
+    // function getUpperCase() {
+    //   var upperCaseOption = "";
+    //   upperCaseOption = console.log(alphabet);
+    // }
+    characterPool = characterPool.concat(alphabet);
+  }
+
+  // If user says yes(true), I need to include a random lower case number
+  if (lowerCaseLetters === true) {
+    // function getLowercase() {
+    //   var lowerCaseOption = "";
+    //   lowerCaseOption = console.log(lowerCaseAlphabet);
+    // }
+    characterPool = characterPool.concat(lowerCaseAlphabet);
+  }
+  console.log(characterPool);
+  // generateCharacterLength()
+
+  // var RandomNumber = getARandomNumber()
+  // var RandomSpecialCharacter = getASpecialCharacter()
+  // var RandomUppercase = getUpperCase()
+  // var RandomLowercase = getLowercase()
+  var finalPassword = [];
+  //generate final password with character length specified 
+  for (var i = 0; i < numberOfCharacters; i++) {
+    // select single random index from characterpool based on the characterpool's current length
+    var getIndex = Math.floor(Math.random() * characterPool.length)
+    //convert random index to true character value
+    var characterValue = characterPool[getIndex];
+    finalPassword.push(characterValue);
+    console.log(finalPassword);
+    // return finalPassword = RandomSpecialCharacter + RandomNumber + RandomUppercase + RandomLowercase;
+  }
+  return finalPassword.join("");
+}
+
+// generatePassword()
+
+// Write password to the #password input
+
